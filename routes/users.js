1ce230a937e49325738e5fcecb260db1db4c78c6
Greg Goforth <greg.goforth@gmail.com>
2016-10-17 19:28:30 -0700

    adds registration form

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/login', (req, res, next) => {
+  res.json(req.body);   
+});
+
+router.get('/register', (req, res, next) => {
+  res.render('register'); 
 });
 
 module.exports = router;
