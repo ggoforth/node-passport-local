449d3e20bdc72ed6095f20dcd07c0d6c87486d9e
Greg Goforth <greg.goforth@gmail.com>
2016-10-17 20:01:23 -0700

    register users

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var express = require('express');
-var router = express.Router();
+var router = express.Router(); 
+let mongoose = require('mongoose'),
+  User = mongoose.model('User');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -14,6 +16,16 @@ router.post('/login', (req, res, next) => {
 
 router.get('/register', (req, res, next) => {
   res.render('register'); 
+});
+
+router.post('/register', (req, res, next) => {
+  //create a new instance of a user model
+  let user = new User(req.body);
+  
+  user.save()
+    .then(() => {
+      res.json(user); 
+    });
 });
 
 module.exports = router;
